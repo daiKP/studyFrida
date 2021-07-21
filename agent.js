05329735e5c228277f0a4e11ace774366008b19f
git <851855760@qq.com>
2021-07-21 22:53:05 +0800

    file

--- agent.js
+++ agent.js
@@ -10,14 +10,14 @@ function init(){
 }
 //get current Activity copid  from objection 
 function getCurrentActivity(){
-    const activityThread = Java.use("android.app.ActivityThread");
-    const activity = Java.use("android.app.Activity");
-    const activityClientRecord = Java.use("android.app.ActivityThread$ActivityClientRecord");
-    const currentActivityThread = activityThread.currentActivityThread();
-    const activityRecords = currentActivityThread.mActivities.value.values().toArray();
+    var activityThread = Java.use("android.app.ActivityThread");
+    var activity = Java.use("android.app.Activity");
+    var activityClientRecord = Java.use("android.app.ActivityThread$ActivityClientRecord");
+    var currentActivityThread = activityThread.currentActivityThread();
+    var activityRecords = currentActivityThread.mActivities.value.values().toArray();
     let currentActivity;
-    for (const i of activityRecords) {
-        const activityRecord = Java.cast(i, activityClientRecord);
+    for (var i of activityRecords) {
+        var activityRecord = Java.cast(i, activityClientRecord);
         if (!activityRecord.paused.value) {
             currentActivity = Java.cast(Java.cast(activityRecord, activityClientRecord).activity.value, activity);
             break;
@@ -40,7 +40,7 @@ function setProxy(addr,port){
 }
 //bypassSuCheck
 function bypassSuCheck(){
-    const commonPaths = [
+    var commonPaths = [
         "/data/local/bin/su",
         "/data/local/su",
         "/data/local/xbin/su",
@@ -56,22 +56,64 @@ function bypassSuCheck(){
         "/system/xbin/su",
     ];
     //detect file Exits
-    const JavaFile=Java.use("Java.io.File");
+    var JavaFile=Java.use("java.io.File");
     JavaFile.exists.implementation=function(){
-        const filename=this.getAbsolutePath();
+        var filename=this.getAbsolutePath();
         if(commonPaths.indexOf(filename)>=0)
             return false;
         return this.exists.call(this);
     }
     //detect */*su
-    const JavaRuntime=Java.use("java.lang.Runtime");
-    const iOException = Java.use("java.io.IOException");
+    var JavaRuntime=Java.use("java.lang.Runtime");
+    var iOException = Java.use("java.io.IOException");
     JavaRuntime.exec.overload("java.lang.String").implementation=function(cmd){
         if(cmd.enddWith("su")){
             throw iOException.$new("anti-root");
         }
         return this.exec.overload("java.lang.String").call(this,cmd);
     }
+}
+//watch file wr behavior
+function watchFileBehavior(){
+    var JavaFile=Java.use("java.io.File");
+    JavaFile.$init.overload("java.lang.String").implementation=function(filePath){
+        console.log("file Object  new"+filePath)
+        var ret=this.$init(filePath);
+        return ret;
+    }
+
+    var javaFileInputStream=Java.use("java.io.FileInputStream");
+    javaFileInputStream.read.overloads.forEach(function(overload){
+        overload.implementation=function(){
+            var filepath=this.path.value;
+            console.log("FileInputStream.read path is: "+filepath);
+            return this.apply(this,arguments);
+        }
+    });
+
+    var javaFileOutputStream=Java.use("java.io.FileOutputStream");
+    javaFileOutputStream.write.overloads.forEach(function(overload){
+        overload.implementation=function(){
+            var filepath=this.path.value;
+            console.log("FileOutputStream.write path is: "+filepath);
+            return overload.call(this,arguments);
+        }
+    });
+    Interceptor.
+    //var FileWrites=JavaFile["write"].overloads;
+    //FileWrites.forEach(function(overload){
+    //    overload.implementation=function(){
+    //        console.log("file write "+this.getAbsolutePath());
+    //        return overload.apply(this,arguments);
+    //    }
+    //});
+    //var FileReads=JavaFile["read"].overloads;
+    //FileReads.forEach(function(overload){
+    //    overload.implementation=function(){
+    //        console.log("file read "+this.getAbsolutePath());
+    //        return overload.apply(this,arguments);
+    //    }
+    //});
 }
 function start(){
     init();
